146f356737c40d9ff2139e0db4495b929c97780a
Densi <Ignatiev1904.ph@mail.ru>
2025-10-15 15:14:10 +0800

    adding comments

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-class Actors(models.Model):
-    actor_name=models.TextField()
+class Actors(models.Model): #в единственном чиле
+    name=models.TextField() #убрать префиксы
 
 class Shows(models.Model):
     title=models.TextField()
@@ -10,23 +10,23 @@ class Genres(models.Model):
     genre_name=models.TextField()
 
 class ShowsGenres(models.Model):
-    show_id=models.ForeignKey(Shows, on_delete=models.CASCADE)
-    genre_id=models.ForeignKey(Genres, on_delete=models.CASCADE)
+    show=models.ForeignKey(Shows, on_delete=models.CASCADE)
+    genre=models.ForeignKey(Genres, on_delete=models.CASCADE) #
 
 class Posters(models.Model):
     date=models.DateField()
+    image=models.ImageField()
 
 class PosterShow(models.Model):
-    show_id=models.ForeignKey(Shows)
+    show=models.ForeignKey(Shows)
     poster_id=models.ForeignKey(Posters)
+    #Добавить дату
+
 
 class PosterActors(models.Model):
     poster_id=models.ForeignKey(Posters)
     actor_id=models.ForeignKey(Actors)
 
-class Users(models.Model):
-    user_name=models.TextField()
-    hash_password=models.TextField()
 
 class Tickets(models.Model):
     date=models.DateField()
@@ -34,6 +34,6 @@ class Tickets(models.Model):
     seat=models.IntegerField()
     status=models.BooleanField()
     show_id=models.ForeignKey(Shows)
-    user_id=models.ForeignKey(Users)
+    user_id=models.ForeignKey()#добавить юзера и его расширение
 
 
