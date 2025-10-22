29bfd02f9e62b5604a80f6bd77d0d900924f3983
Densi <Ignatiev1904.ph@mail.ru>
2025-10-22 14:32:05 +0800

    adding userProfile and refactor models

--- main/models.py
+++ main/models.py
@@ -1,39 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Actors(models.Model): #в единственном чиле
+class Actor(models.Model): #в единственном чиле
     name=models.TextField() #убрать префиксы
 
-class Shows(models.Model):
+class Show(models.Model):
     title=models.TextField()
-
-class Genres(models.Model):
+class Genre(models.Model):
     genre_name=models.TextField()
 
 class ShowsGenres(models.Model):
-    show=models.ForeignKey(Shows, on_delete=models.CASCADE)
-    genre=models.ForeignKey(Genres, on_delete=models.CASCADE) #
-
-class Posters(models.Model):
+    show=models.ForeignKey(Show, on_delete=models.CASCADE)
+    genre=models.ForeignKey(Genre, on_delete=models.CASCADE) #
+class Poster(models.Model):
     date=models.DateField()
     image=models.ImageField()
 
 class PosterShow(models.Model):
-    show=models.ForeignKey(Shows)
-    poster_id=models.ForeignKey(Posters)
-    #Добавить дату
+    show=models.ForeignKey(Show)
+    poster_id=models.ForeignKey(Poster)
+    date=models.DateField()
+class PosterActor(models.Model):
+    poster=models.ForeignKey(Poster)
+    actor=models.ForeignKey(Actor)
 
+class UserProfile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    passport_number=models.IntegerField()
+    passport_series=models.IntegerField()
+    full_name=models.CharField()
+    birth_date=models.DateField()
 
-class PosterActors(models.Model):
-    poster_id=models.ForeignKey(Posters)
-    actor_id=models.ForeignKey(Actors)
-
-
-class Tickets(models.Model):
+class Ticket(models.Model):
     date=models.DateField()
     row=models.IntegerField()
     seat=models.IntegerField()
     status=models.BooleanField()
-    show_id=models.ForeignKey(Shows)
-    user_id=models.ForeignKey()#добавить юзера и его расширение
+    show=models.ForeignKey(Show)
+    user=models.ForeignKey(UserProfile)
 
 
