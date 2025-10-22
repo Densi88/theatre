979530400cdda7d32e090a8a6bdf82b5114243fd
Densi <Ignatiev1904.ph@mail.ru>
2025-10-22 15:06:58 +0800

    adding models for admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+@admin.register(Ticket)
+class AdminTicket(admin.ModelAdmin):
+    pass
+
+@admin.register(Actor)
+class AdminActor(admin.ModelAdmin):
+    pass
+
+@admin.register(Show)
+class AdminShow(admin.ModelAdmin):
+    pass
+
+@admin.register(PosterShow)
+class AdminPosterShow(admin.ModelAdmin):
+    pass
+
+@admin.register(PosterActor)
+class AdminPosterActor(admin.ModelAdmin):
+    pass
+
+@admin.register(UserProfile)
+class AdminUserProfile(admin.ModelAdmin):
+    pass
+
+
--- main/models.py
+++ main/models.py
@@ -1,35 +1,59 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Actor(models.Model): #в единственном чиле
-    name=models.TextField() #убрать префиксы
+class Actor(models.Model): 
+    name=models.TextField()
+    class Meta:
+        verbose_name="Актер"
+        verbose_name_plural="Актеры" 
 
 class Show(models.Model):
-    title=models.TextField()
+    title=models.TextField("Название")
+
+
 class Genre(models.Model):
-    genre_name=models.TextField()
+    genre_name=models.TextField("Название")
+    class Meta:
+        verbose_name="Жанр"
+        verbose_name_plural="Жанры"
+
+
 
 class ShowsGenres(models.Model):
     show=models.ForeignKey(Show, on_delete=models.CASCADE)
-    genre=models.ForeignKey(Genre, on_delete=models.CASCADE) #
+    genre=models.ForeignKey(Genre, on_delete=models.CASCADE) 
+
+
 class Poster(models.Model):
     date=models.DateField()
     image=models.ImageField()
+    class Meta:
+        verbose_name="Афиша"
+        verbose_name_plural="Афиши"
+
+
 
 class PosterShow(models.Model):
     show=models.ForeignKey(Show)
     poster_id=models.ForeignKey(Poster)
     date=models.DateField()
+
+
 class PosterActor(models.Model):
     poster=models.ForeignKey(Poster)
     actor=models.ForeignKey(Actor)
+
 
 class UserProfile(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
     passport_number=models.IntegerField()
     passport_series=models.IntegerField()
-    full_name=models.CharField()
+    full_name=models.TextField("ФИО")
     birth_date=models.DateField()
+    class Meta:
+        verbose_name="Пользователь"
+        verbose_name_plural="Пользователи"
+
 
 class Ticket(models.Model):
     date=models.DateField()
@@ -38,5 +62,8 @@ class Ticket(models.Model):
     status=models.BooleanField()
     show=models.ForeignKey(Show)
     user=models.ForeignKey(UserProfile)
+    class Meta:
+        verbose_name="Билет"
+        verbose_name_plural="Билеты"
 
 
